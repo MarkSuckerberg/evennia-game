2eeb908cdfa529645835663a3ba93fb35519fc7b
Majorhalowars <54008308+Majorhalowars@users.noreply.github.com>
2022-09-10 17:03:29 -0600

    Update characters.py

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -39,6 +39,6 @@ class Character(DefaultCharacter):
     def return_appearance(self, looker):
         "This is going to be used for like, custom descriptions n stuff. Until i setup that though, just an hp counter"
         text = super().return_appearance(looker)
-        text = self.db.currentHP + "/" + self.db.maxHp
+        text = str(self.db.currentHP) + "/" + str(self.db.maxHp)
         return text
         
